545d14eb30c6f054c40efe3e0b39aadd56a389c6
Yurii <pazyn404@gmail.com>
2022-11-21 19:45:43 +0200

    Fix ALLOWED_HOSTS

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") == "True"
 
-ALLOWED_HOSTS = ["https://todo-list-yk6u.onrender.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "todo-list-yk6u.onrender.com"]
 
 INTERNAL_IPS = ["127.0.0.1"]
 
